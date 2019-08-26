266245427db05b19a151a990733174b3dc2a4f76
addisonpohl <addison.c.pohl@gmail.com>
2019-08-26 00:14:34 -0500

    Add switch statments syntax

---
+++ switch_statment_syntax.js
@@ -0,0 +1,41 @@
+function caseInSwitch(val) {
+    var answer = "";
+    switch(val) {
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "omega";
+            break;
+    }
+    return answer
+}
+console.log(caseInSwitch(1));
+
+
+//Multiple identical values in a switch statment
+function sequentialSizes(val) {
+    var answer = ""
+    switch(val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High";
+            break;
+    }
+    return answer;
+}
+console.log(sequentialSizes(3));

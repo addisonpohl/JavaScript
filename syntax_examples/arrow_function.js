b5e8e24cd7f8ff84967747d2f0d23e69cb7e0f82
addisonpohl <addison.c.pohl@gmail.com>
2019-09-09 23:49:36 -0500

    Moved Object syntax and arrow function to examples

---
+++ syntax_examples/arrow_function.js
@@ -0,0 +1,10 @@
+var myConcat = function(arr1, arr2) {
+  return arr1.concat(arr2);
+};
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+//arrow functions example
+const arrowConcat = (arr1, arr2) => arr1.concat(arr2);
+console.log(arrowConcat([10, 11], [12, 13]));
+
